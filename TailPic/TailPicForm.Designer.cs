586ed94511ba520820c1a925abb009b18af60991
kevinpluckivendi <kevin.pluck@ivendi.com>
2013-06-07 15:17:41 +0100

    removed orphaned component

--- TailPic/TailPicForm.Designer.cs
+++ TailPic/TailPicForm.Designer.cs
@@ -28,21 +28,9 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.tailPictureBox1 = new TailPic.TailPictureBox();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.tailPictureBox1)).BeginInit();
             this.SuspendLayout();
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(282, 253);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
-            this.pictureBox1.TabIndex = 1;
-            this.pictureBox1.TabStop = false;
             // 
             // tailPictureBox1
             // 
@@ -62,14 +50,12 @@
             this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.ClientSize = new System.Drawing.Size(282, 253);
             this.Controls.Add(this.tailPictureBox1);
-            this.Controls.Add(this.pictureBox1);
             this.HelpButton = true;
             this.MaximizeBox = false;
             this.Name = "TailPicForm";
             this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
             this.Text = "TailPic";
             this.Shown += new System.EventHandler(this.TailPicForm_Shown);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.tailPictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -78,7 +64,6 @@
 
         #endregion
 
-        private System.Windows.Forms.PictureBox pictureBox1;
         private TailPictureBox tailPictureBox1;
     }
 }
